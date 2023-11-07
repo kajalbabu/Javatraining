c192e008e7779761b22382f15c33d55fbd518a19
kajalbabu <kajal.b@marketsimplified.com>
2023-11-07 16:45:33 +0530

    New program for thread

---
+++ thread.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+class A implements Runnable{
+    public void run(){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter 1st string:");
+    String str1=sc.nextLine();
+    for(int i=0;i<5;i++){
+        System.out.println(str1);
+        try {
+            Thread.sleep(500);
+        } catch (Exception e) {
+            System.out.println("Exception happened");
+        }
+        
+    }
+}
+}
+class B implements Runnable{
+    public void run(){
+    Scanner sc2 = new Scanner(System.in);
+    System.out.println("Enter 2nd string:");
+    String str2=sc2.nextLine();
+    for(int i=0;i<5;i++){
+        System.out.println(str2);
+        try {
+            Thread.sleep(500);
+        } catch (Exception e) {
+           System.out.println("Exception happened");
+        }
+        
+    }
+}
+}
+
+
+public class thread {
+    public static void main(String [] args){
+        A obj1=new A();
+        B obj2=new B();
+        Thread thread=new Thread(obj1);
+        Thread thread2=new Thread(obj2);
+        thread.start();
+        thread2.start();
+    }
+}
